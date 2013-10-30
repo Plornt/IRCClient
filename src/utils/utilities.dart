191e2dece9f7b2b69991b0ace9e758b1d51754e1
Thomas Caserta <tomcaserta@gmail.com>
2013-10-30 18:28:48 +0000

    Missed one...

--- src/utils/utilities.dart
+++ src/utils/utilities.dart
@@ -13,25 +13,6 @@ class ClientCommand {
   }
 }
 
-/** 
- * 3.1.1 Password message
- *   
- *   Command: PASS
- *   Parameters: <password>
- *   
- *   The PASS command is used to set a 'connection password'.  The
- *   optional password can and MUST be set before any attempt to register
- *   the connection is made.  Currently this requires that user send a
- *   PASS command before sending the NICK/USER combination.
- *   
- *   Numeric Replies:
- *   
- *     ERR_NEEDMOREPARAMS - ERR_ALREADYREGISTRED
- *     
- *   Example:
- *   
- *      - PASS secretpasswordhere
- */
 class PassCommand extends ClientCommand {
   String password = "";
   PassCommand (this.password) {
